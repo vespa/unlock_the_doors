c6a792180bbdb75a21914fb1e23a984647b4b13d
vespa <d.vespa@gmail.com>
2014-11-02 17:34:05 -0200

    testes de movimentação

---
+++ Assets/scripts/commons/CharacterMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Commons;
+
+public class CharacterMovement : MouseEvents {
+	private bool activeMovement = false;
+
+	private Vector2 start;
+	private Vector2 end;
+	public float spacesWalkedPerTime = 1f;
+	private string direcao = "longe";
+
+	public void DeactiveMovement(){
+		activeMovement = false;
+		var pos = transform.position;
+		pos.x = Mathf.Round(pos.x);
+		pos.y = Mathf.Round(pos.y);
+		transform.position= pos;
+	}
+	public void ActiveMovement(){
+		activeMovement = true;
+		Invoke ("DeactiveMovement", 1);
+	}
+	public void Movement(){
+		end.x = 0f;
+		end.y = 0f;
+
+		if(direcao != "longe"){
+			if(direcao == "baixo")
+				end.y = -1f;
+
+			if(direcao == "cima")
+				end.y = 1f;
+
+			if(direcao == "esquerda")
+				end.x = -1f;
+
+			if(direcao == "direita")
+				end.x = 1f;
+
+			var mov = Vector2.Lerp(start, end, Time.deltaTime * spacesWalkedPerTime);
+			transform.Translate(mov);
+		}
+	}
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && !activeMovement) {
+			Vector3 pos = Input.mousePosition;
+			direcao = GetMousePosition(pos);
+			ActiveMovement();
+		}
+		if(activeMovement)
+			Movement();
+	}
+}
---
+++ Assets/scripts/commons/MouseEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets.Scripts.Commons
+{
+	public class MouseEvents : MonoBehaviour {
+		public float distance = 2f;
+		public string GetMousePosition(Vector3 mousePosition){
+			var pos = Camera.main.ScreenToWorldPoint(mousePosition);
+			var stat  = transform.position;
+
+			string posx = "";
+			string posy = "";
+			string direcao = "";
+			float horizontal = stat.x - pos[0];
+			float vertical = stat.y - pos[1];
+			if((vertical> -distance && vertical< distance) && (horizontal> -distance && horizontal< distance)){
+				posx = (horizontal > 0)? "esquerda":"direita";
+				posy = (vertical < 0)? "cima":"baixo";
+				horizontal = (horizontal > 0)? horizontal : -horizontal;
+				vertical = (vertical > 0)? vertical : -vertical;
+				direcao = (horizontal < vertical)?posy:posx;
+				return direcao;
+			}else {
+				return "longe";
+			}
+		}
+
+	}
+}
